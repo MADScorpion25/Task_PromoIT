fbce4ca90f375edf52653215f0134e84c3e10db1
admal <ad.malkova73@gmail.com>
2022-10-13 10:46:44 +0400

    clear main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,9 @@
 package com.company;
 
-import java.io.IOException;
+import java.util.*;
 
-public class Main
-{
+public class Main{
     public static void main(String[] args) {
-        ApiConnection apiConnection = new ApiConnection();
-        String phrase;
-        try {
-            phrase = apiConnection.getRequestString();
-        } catch (IOException e) {
-            phrase = "";
-        }
 
-        System.out.println(apiConnection.getUrl());
-        System.out.println(phrase);
-
-        FrequencyDictionary frequencyDictionary = new FrequencyDictionary(phrase);
-
-        System.out.println("Частоты:");
-        frequencyDictionary.computeFrequencies()
-                .forEach((key, value) -> System.out.println(key + " - " + value));
-
-        frequencyDictionary.computeAverageFrequency();
-
-        System.out.println("Среднее значение частоты: " + frequencyDictionary.getLettersAndDigitsCount()
-                + " / " + frequencyDictionary.getFrequencies().size()
-                + " = "
-                + frequencyDictionary.getAverageFrequency());
-
-        System.out.println("Символы, наиболее близкие к среднему значению частоты:");
-        frequencyDictionary.getAverageFrequentSymbols()
-                .forEach(symbol -> System.out.print(symbol + "(" + (int)symbol + ") "));
     }
 }
