0f743e958d7fba17081e30306ecf45ab6556e751
admal <ad.malkova73@gmail.com>
2022-12-05 19:41:09 +0400

    configuration set

---
+++ src/main/java/com/dealerapp/config/MvcConfig.java
@@ -0,0 +1,94 @@
+package com.dealerapp.config;
+
+import com.dealerapp.config.jwt.AuthEntryPointJwt;
+import com.dealerapp.config.jwt.AuthTokenFilter;
+import com.dealerapp.models.enums.UserRole;
+import com.dealerapp.services.UserDetailsServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebMvc
+@EnableWebSecurity
+public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String path;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt authEntryPointJwt;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:" + path + "/");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/*").not().fullyAuthenticated()
+                .antMatchers("/orders/edit/:id", "/orders/*", "/configurations/transmission-types", "/configurations/drive-types", "/configurations/body-types", "/configurations/car-classes", "/configurations/edit/:id", "/configurations/*", "/cars/edit/:id", "/cars/*").hasAuthority(UserRole.DEALER.name())
+                .antMatchers("/users/*", "/users/*/*", "/auth/*").hasAuthority(UserRole.ADMIN.name())
+                .antMatchers("/orders/edit/:id", "/orders/*").hasAuthority(UserRole.CLIENT.name())
+                .antMatchers("/img/*", "/auth/*", "/", "/reviews", "/reviews/:id", "/cars/info/:id", "/cars", "/logout").permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .logoutSuccessUrl("/auth/sign-in")
+                .and()
+                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsServiceImpl userDetailService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
---
+++ src/main/java/com/dealerapp/config/jwt/JwtUtils.java
@@ -0,0 +1,40 @@
+package com.dealerapp.config.jwt;
+
+import com.dealerapp.security.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication){
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public boolean validateToken(String jwt){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt);
+            return true;
+        }catch(MalformedJwtException | IllegalArgumentException ex){
+            System.err.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String jwt){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt).getBody().getSubject();
+    }
+}
